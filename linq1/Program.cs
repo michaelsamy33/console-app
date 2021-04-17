b9b7fc1767b230c0a5fe1f63d995cf75e619f1ad
unknown <Kirollousmagdy3333@gmail.com>
2021-04-17 22:20:05 +0200

    add file

---
+++ linq1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linq1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] Students = new string[] { "ahlam", "ali", "mohmed", "kirollous", "sara", "mariam", "hosnya", "magdy" };
+
+            var Result = from student in Students
+                         orderby student descending
+                         select student;
+
+
+            foreach (var s in Result)
+            {
+
+                Console.WriteLine(s);
+            }
+
+
+
+        }
+    }
+}
